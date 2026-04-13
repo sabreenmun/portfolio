ebe067e3900be91adf74119dfdbfb4aebec79299
sabreenmun <sabreenyo@gmail.com>
2026-04-12 21:39:59 -0400

    fix navbar mobile layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,23 @@
-import { Link } from "react-router-dom";
-import { FaInstagram, FaLinkedin } from "react-icons/fa";
-
 const Navbar = () => {
   return (
-    <nav className="relative flex items-center justify-between px-10 py-6">
-      {/* LEFT */}
-      <div className="flex gap-6 text-sm">
+    <nav className="flex flex-col gap-4 px-4 py-4 md:flex-row md:items-center md:justify-between">
+      
+      {/* LOGO */}
+      <div className="text-center text-lg font-medium tracking-wide md:text-left">
+        <Link to="/">Sabreen Munassar</Link>
+      </div>
+
+      {/* LINKS */}
+      <div className="flex flex-wrap justify-center gap-4 text-sm">
         <Link to="/photography">Photography</Link>
         <Link to="/other">Other Work</Link>
         <Link to="/guide">Guide</Link>
         <Link to="/about">About</Link>
       </div>
 
-      {/* CENTER */}
-      <div className="absolute left-1/2 -translate-x-1/2 text-lg font-medium tracking-wide">
-        <Link to="/">Sabreen Munassar</Link>
-      </div>
-
-      {/* RIGHT */}
-      <div className="flex gap-6 text-sm">
-       <a
+      {/* ICONS */}
+      <div className="flex justify-center gap-4 md:justify-end">
+        <a
           href="https://instagram.com/sabreennnnnn"
           target="_blank"
           rel="noreferrer"
@@ -34,8 +32,8 @@ const Navbar = () => {
           <FaLinkedin className="hover:opacity-60 transition" size={20} />
         </a>
       </div>
+
     </nav>
   );
 };
-
 export default Navbar;
